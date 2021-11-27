c306facffd5a49c2568292b52d64d55905450789
alanparr <alanjparr83@gmail.com>
2021-11-27 23:48:04 +0000

    Added FromPercentage for TaxRate and a few basic tests

--- NPrice.Tests.Unit/TaxRateTests.cs
+++ NPrice.Tests.Unit/TaxRateTests.cs
@@ -13,5 +13,23 @@ namespace NPrice.Tests.Unit
             Assert.Equal(expectedZeroPrefixValue, taxRate.GetAsFractionOfZero());
             Assert.Equal(expectedOnePrefixValue, taxRate.GetAsFractionOfOne());
         }
+
+        [Theory]
+        [InlineData(20, 0.2)]
+        [InlineData(5, 0.05)]
+        public void FromDecimalPercentage(decimal percentage, decimal expectedRateValue)
+        {
+            var result = TaxRate.FromPercentage(percentage);
+            Assert.Equal(expectedRateValue, result.GetAsFractionOfZero());
+        }
+
+        [Theory]
+        [InlineData(20d, 0.2)]
+        [InlineData(5d, 0.05)]
+        public void FromDoublePercentage(double percentage, decimal expectedRateValue)
+        {
+            var result = TaxRate.FromPercentage(percentage);
+            Assert.Equal(expectedRateValue, result.GetAsFractionOfZero());
+        }
     }
 }
--- NPrice/TaxRate.cs
+++ NPrice/TaxRate.cs
@@ -1,4 +1,6 @@
-﻿namespace NPrice
+﻿using System;
+
+namespace NPrice
 {
     public class TaxRate
     {
@@ -10,5 +12,19 @@
 
         public decimal GetAsFractionOfZero() => _value > 1 ? _value - 1 : _value;
         public decimal GetAsFractionOfOne() => _value < 1 ? _value + 1 : _value;
+
+        public static TaxRate FromPercentage(decimal percentageValue)
+        {
+            if (percentageValue == 0m) return new TaxRate(percentageValue);
+
+            return new TaxRate(percentageValue / 100);
+        }
+
+        public static TaxRate FromPercentage(double percentageValue)
+        {
+            if (percentageValue == 0d) return new TaxRate(Convert.ToDecimal(percentageValue));
+
+            return new TaxRate(Convert.ToDecimal(percentageValue) / 100);
+        }
     }
 }
