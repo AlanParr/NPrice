8412aa9a969ceee69a6b935d8b7fd307a47867f1
alanparr <alanjparr83@gmail.com>
2022-02-15 20:03:20 +0000

    Updated usage test

--- NPrice.Tests.Unit/UsageTests.cs
+++ NPrice.Tests.Unit/UsageTests.cs
@@ -1,22 +1,41 @@
-﻿namespace NPrice.Tests.Unit;
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace NPrice.Tests.Unit;
 
 /// <summary>
 /// These tests are not so much to test behavior as to test the usability of the API.
 /// </summary>
 public class UsageTests
 {
+    private readonly ITestOutputHelper _outputHelper;
+
+    public UsageTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+    
+    [Fact]
     public void CreateLinkedPriceFromScratch()
     {
         var databaseEntity = new
         {
-            Price = 24, Currency = "GBP"
+            Price = 24.675m, Currency = "GBP"
         };
 
         var price = new CurrencyLinkedNetPrice(databaseEntity.Price, 2, new Currency(databaseEntity.Currency));
-        
+        MethodThatIsExpectingANetPrice(price);
         //Thoughts:
         // Should there be an implicit conversion from string to Currency?
         //This would make the above:
         // var price = new CurrencyLinkedNetPrice(databaseEntity.Price, 2, databaseEntity.Currency);
+        
+        //Just to make it pass, change to false if test is showing an undesirable pattern of usage.
+        Assert.True(true);
+    }
+
+    private void MethodThatIsExpectingANetPrice(NetPrice netPrice)
+    {
+        _outputHelper.WriteLine($"Object of type {netPrice.GetType()} received with value {netPrice.Value}");
     }
 }
